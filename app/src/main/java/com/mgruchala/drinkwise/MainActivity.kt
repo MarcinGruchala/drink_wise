c110ebc7e7f0c903e8e977a4ac66b03e2b866b2b
Marcin Gruchala <margruchala@gmail.com>
2025-02-16 12:20:55 +0100

    feat: Setup app top level navigation

--- app/src/main/java/com/mgruchala/drinkwise/MainActivity.kt
+++ app/src/main/java/com/mgruchala/drinkwise/MainActivity.kt
@@ -5,10 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.mgruchala.drinkwise.calculator.AlcoholCalculatorView
+import com.mgruchala.drinkwise.calendar.CalendarScreen
+import com.mgruchala.drinkwise.home.HomeScreen
+import com.mgruchala.drinkwise.settings.SettingsScreen
 import com.mgruchala.drinkwise.ui.theme.DrinkWiseTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +33,107 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DrinkWiseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
-                    AlcoholCalculatorView()
-                }
+                val navController = rememberNavController()
+                var bottomNavSelectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar {
+                            bottomNavigationItems.forEachIndexed { index, bottomNavigationItem ->
+                                NavigationBarItem(
+                                    selected = index == bottomNavSelectedItemIndex,
+                                    onClick = {
+                                        bottomNavSelectedItemIndex = index
+                                        navController.navigate(bottomNavigationItem.route)
+                                    },
+                                    icon = {
+                                        val iconAssetId = if (index == bottomNavSelectedItemIndex) {
+                                            bottomNavigationItem.activeIconAssetId
+                                        } else {
+                                            bottomNavigationItem.inactiveIconAssetId
+                                        }
+                                        Icon(
+                                            painter = painterResource(iconAssetId),
+                                            contentDescription = bottomNavigationItem.name
+                                        )
+                                    },
+                                    label = {
+                                        Text(bottomNavigationItem.name)
+                                    },
+                                )
+
+                            }
+                        }
+                    },
+                    content = { innerPadding ->
+                        NavHost(
+                            navController,
+                            startDestination = HomeRoute,
+                            Modifier.padding(innerPadding)
+                        ) {
+                            composable<HomeRoute> {
+                                HomeScreen()
+                            }
+                            composable<CalendarRoute> {
+                                CalendarScreen()
+                            }
+                            composable<CalculatorRoute> {
+                                AlcoholCalculatorView()
+                            }
+                            composable<SettingsRoute> {
+                                SettingsScreen()
+                            }
+
+                        }
+                    }
+                )
             }
         }
     }
 }
+
+@Serializable
+object HomeRoute
+
+@Serializable
+object CalendarRoute
+
+@Serializable
+object CalculatorRoute
+
+@Serializable
+object SettingsRoute
+
+data class BottomNavigationItem<T : Any>(
+    val name: String,
+    val route: T,
+    val activeIconAssetId: Int,
+    val inactiveIconAssetId: Int
+)
+
+val bottomNavigationItems = listOf(
+    BottomNavigationItem(
+        name = "Home",
+        route = HomeRoute,
+        activeIconAssetId = R.drawable.ic_home_filled,
+        inactiveIconAssetId = R.drawable.ic_home
+    ),
+    BottomNavigationItem(
+        name = "Calendar",
+        route = CalendarRoute,
+        activeIconAssetId = R.drawable.ic_calendar_filled,
+        inactiveIconAssetId = R.drawable.ic_calendar
+    ),
+    BottomNavigationItem(
+        name = "Calculator",
+        route = CalculatorRoute,
+        activeIconAssetId = R.drawable.ic_calculate_filled,
+        inactiveIconAssetId = R.drawable.ic_calculate
+    ),
+    BottomNavigationItem(
+        name = "Settings",
+        route = SettingsRoute,
+        activeIconAssetId = R.drawable.ic_settings_filled,
+        inactiveIconAssetId = R.drawable.ic_settings
+    )
+)
---
+++ app/src/main/java/com/mgruchala/drinkwise/settings/SettingsScreen.kt
@@ -0,0 +1,14 @@
+package com.mgruchala.drinkwise.settings
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen() {
+    Box {
+        Text(
+            text = "Settings screen",
+        )
+    }
+}
