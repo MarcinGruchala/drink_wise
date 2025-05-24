9e008c4f7cc13e1f518234766756ec574dd7c4f9
Marcin Gruchala <margruchala@gmail.com>
2025-05-24 13:46:19 +0200

    refactor: Update bottom navigation to use string resources for item names

--- app/src/main/java/com/mgruchala/drinkwise/navigaiton/AppNavigation.kt
+++ app/src/main/java/com/mgruchala/drinkwise/navigaiton/AppNavigation.kt
@@ -1,13 +1,16 @@
 package com.mgruchala.drinkwise.navigaiton
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -48,13 +51,13 @@ fun AppNavigation() {
                             } else {
                                 bottomNavigationItem.inactiveIconAssetId
                             }
-                            androidx.compose.material3.Icon(
+                            Icon(
                                 painter = painterResource(iconAssetId),
-                                contentDescription = bottomNavigationItem.name
+                                contentDescription = stringResource(bottomNavigationItem.nameRes)
                             )
                         },
                         label = {
-                            androidx.compose.material3.Text(bottomNavigationItem.name)
+                            Text(stringResource(bottomNavigationItem.nameRes))
                         }
                     )
                 }
